9010d70c9075a667f423fdc4e2e5dd21b7ef30e3
余珩 <1281005689@qq.com>
2018-10-21 20:39:29 +0800

    [Login] Click login button to redirect to home page

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,28 +1,60 @@
 import React from 'react';
+import {
+  object,
+} from 'prop-types';
 import Button from 'Common/Button';
 import TextField from 'Common/TextField';
 import classes from './index.less';
 
 class Login extends React.Component {
+  static propTypes = {
+    history: object.isRequired,
+  };
+
   state = {
     password: '',
     userName: '',
   };
 
-  handleClick = (event) => {
-    console.log(this.state);
+  handleClick = () => {
+    const {
+      history,
+    } = this.props;
+
+    const {
+      password,
+      userName,
+    } = this.state;
+  
+    fetch('http://localhost:3000/login', {
+      headers: {
+        // 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+      },
+      method: 'POST',
+      mode: 'cors',
+      // body: `password=${password}&userName=${userName}`,
+      body: JSON.stringify({
+        password,
+        userName,
+      }),
+    })
+      .then((response) => {
+        // console.log(response);
+        history.push('/home');
+      })
+      .catch((err) => {
+        // console.log(err);
+      });
   }
 
   handlePasswordChange = (event) => {
     this.setState({
-      ...this.state,
       password: event.target.value,
     });
   }
 
   handleUsernameChange = (event) => {
     this.setState({
-      ...this.state,
       userName: event.target.value,
     });
   }
